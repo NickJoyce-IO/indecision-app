c3c34832a72b5c15f9615d746fd14f81805387ab
Nick Joyce <brawlyrox@gmail.com>
2019-02-04 19:48:26 +0000

    Playing about with JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,61 @@
 console.log('app.js is running');
 
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'More info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var nickTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Ya'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'ya'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'yaaaaaaaaa'
+    )
+);
+
+// Get the root element
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// render the JSX
+ReactDOM.render(nickTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 console.log('app.js is running')
 
-const template = <h1>Indecision App</h1> 
+const template = (
+<div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <p>More info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+)
+
+const nickTemplate = (
+    <div>
+        <h1>Ya</h1>
+        <p>ya</p>
+        <p>yaaaaaaaaa</p>
+    </div>
+)
+
+// Get the root element
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+// render the JSX
+ReactDOM.render(nickTemplate, appRoot)
