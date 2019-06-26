07a216700753b8792017aebb7cf3da5a5600d33a
Nick Joyce <brawlyrox@gmail.com>
2019-06-26 08:26:18 +0100

    tidy up and add comments

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -3,29 +3,31 @@ const app = document.getElementById('app')
 
 let visibility = false
 
+// flip visibility to the opposite of what is currently is
 const toggleVisibility = () => {
-  visibility = !visibility
-  renderTemplate()
+    visibility = !visibility
+    renderTemplate()
 }
 
-const renderTemplate = () => {
+const renderTemplate = () => {ß
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
 
+            {
+                // and operator syntax to show only when visibility is set to true
+            }
+            {visibility &&
 
-const template = (
-    <div>
-    <h1>Visibility Toggle</h1>
-    <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
+                <p>I am now showing stuff</p>
 
-    {visibility && 
-        
-        <p>I am now showing stuff</p>
-        
-    }
-    
-   </div>
-)
+            }
 
-ReactDOM.render(template, app)
+        </div>
+    )
+
+    ReactDOM.render(template, app)
 }
 
 renderTemplate()
