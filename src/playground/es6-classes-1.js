42d60905c983bfaaf2a7c7d863e6832594b962ef
Nick Joyce <brawlyrox@gmail.com>
2019-07-11 08:21:36 +0100

    messing with react component for header

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,8 +12,48 @@ class Person {
     }
 }
 
-const me = new Person('Nick Joyce', 34)
-console.log(me.getDescription())
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major
+    }
+    hasMajor() {
+        return !!this.major
+    }
+    getDescription() {
+        let description = super.getDescription()
+        
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}`
+        }
+        
+        return description
+    }
+}
 
-const other = new Person()
-console.log(other.getDescription())
+const me = new Student('Nick Joyce', 34, 'Computer Science')
+// console.log(me.getDescription())
+
+const other = new Student()
+// console.log(other.hasMajor())
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age) 
+        this.homeLocation = homeLocation
+    }
+    getGreeting() {
+        let greeting = super.getGreeting() 
+        if(this.homeLocation) {
+            greeting += ` I am visiting from ${this.homeLocation}`
+           
+        }
+        return greeting 
+    }
+}
+
+const traveller = new Traveller('Nick Joyce', 34, 'Bath')
+console.log(traveller.getGreeting())
+
+const traveller2 = new Traveller('anonymous', 'anonymous', 'nowhere')
+console.log(traveller2.getGreeting())
---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,71 @@
+console.log('app.js is running')
+
+
+// app object sets up the app
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+}
+
+// get the div that will contain the template
+const appRoot = document.getElementById('app')
+
+// adds options to the app.options array, prevents default behaviour
+// only do this if there is a value in the box
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    const option = e.target.elements.option.value
+
+    if (option) {
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        renderTemplate()
+    }
+
+}
+
+// Removes options, by clearing the array
+const onRemoveAll = () => {
+    app.options = []
+    renderTemplate()
+}
+
+// creates a random number between 0 and the length of the array
+// then returns the option at the index of that random number
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length)
+    const option = app.options[randomNum]
+    alert(option)
+}
+
+
+const renderTemplate = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
+
+            <ol>
+                {app.options.map((option) => <li key={option}>{option}</li>)}
+            </ol>
+
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button>Add option</button>
+
+            </form>
+
+        </div>
+    )
+
+    ReactDOM.render(template, appRoot)
+}
+
+
+renderTemplate()
+
+
