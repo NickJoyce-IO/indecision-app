6303d00e33ab56082c891852c4c93e2ab3ea8730
Nick Joyce <brawlyrox@gmail.com>
2019-07-12 07:54:05 +0100

    sorting out the components and the props passed in

--- src/app.js
+++ src/app.js
@@ -1,18 +1,72 @@
-class Header extends React.Component {
-    render () {
-        return <p>This is from header</p>
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision'
+        const subtitle = 'Put your life in the hands of a computer'
+        const options = ['Thing one', ' Thing two', 'Thing four']
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options options={options} />
+                <AddOption />
+            </div>
+        )
     }
 }
 
-const jsx = (
-    <div>
-        <h1>Title</h1>
-        <Header />
-        <Header />
-        <Header />
-        <Header />
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        )
+    }
+}
 
-    </div>
-)
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'))
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.options.map((option) => <Option key={option} optionText={option} />)}
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                Option: {this.props.optionText}
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                Add option component here
+            </div>
+        )
+    }
+}
+
+
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
