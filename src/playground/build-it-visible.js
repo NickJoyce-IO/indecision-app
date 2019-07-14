5661c6097cac13c708f5a3bee3d25dfbf2979e42
Nick Joyce <brawlyrox@gmail.com>
2019-07-14 17:43:50 +0100

    converting to React class

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,33 +1,37 @@
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+        this.state = {
+            visibility: false
+        }
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !this.state.visibility
+            }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
 
-const app = document.getElementById('app')
+                {
+                    // and operator syntax to show only when visibility is set to true
+                }
+                {this.state.visibility &&
 
-let visibility = false
+                    <p>I am now showing stuff</p>
 
-// flip visibility to the opposite of what is currently is
-const toggleVisibility = () => {
-    visibility = !visibility
-    renderTemplate()
+                }
+
+            </div>
+        )
+    }
 }
 
-const renderTemplate = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
 
-            {
-                // and operator syntax to show only when visibility is set to true
-            }
-            {visibility &&
-
-                <p>I am now showing stuff</p>
-
-            }
-
-        </div>
-    )
-
-    ReactDOM.render(template, app)
-}
-
-renderTemplate()
