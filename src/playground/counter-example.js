6d21a34bd9fb816d4fc5284941ef7777b44daa9b
Nick Joyce <brawlyrox@gmail.com>
2019-07-20 09:22:42 +0100

    add lifecycle hooks to counter to store count to local storage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,24 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.state = {
-            count: props.count
+            count: 0
+        }
+    }
+    componentDidMount() {
+        try {
+            const count = parseInt(localStorage.getItem('count'),10)
+            
+            if(!isNaN(count)) {
+                this.setState(()=> ({count}))
+            }
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count)
         }
     }
     handleAddOne() {
@@ -47,7 +64,7 @@ Counter.defaultProps = {
 
 
 
-ReactDOM.render(<Counter count={100000}/>, document.getElementById('app'))
+ReactDOM.render(<Counter />, document.getElementById('app'))
 
 
 // let count = 0
